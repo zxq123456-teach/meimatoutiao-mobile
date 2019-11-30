c362351d6c0fe9869c33089dc6b57c90f60d4a0f
zxq123456-teach <1060604915@qq.com>
2019-11-30 18:47:54 +0800

    项目初始化-配置rem适配及基础值

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <!-- 根路由出口 -->
     <router-view />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
@@ -22,5 +27,9 @@ export default {
 </script>
 
 <style scoped>
-
+.box {
+  width: 375px;
+  height: 100px;
+  background-color: #f40;
+}
 </style>
