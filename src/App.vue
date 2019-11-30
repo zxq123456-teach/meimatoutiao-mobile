0a8a5b26031b88c9b19f4e73d2342b740161fd16
zxq123456-teach <1060604915@qq.com>
2019-11-30 17:54:18 +0800

    项目初始化-导入Vant

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- 根路由出口 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {
+  },
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
