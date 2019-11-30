4e55abfabee50dfa5176f71beadf98b196487361
zxq123456-teach <1060604915@qq.com>
2019-11-30 19:33:14 +0800

    项目初始化-处理后端返回数据超出JS安全整数范围问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,12 +2,18 @@
  * 封装 axios 请求模块
  */
 import axios from 'axios'
-
+import jsonBig from 'json-bigint'
 // axios.create 方法：复制一个 axios
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
-
+request.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch (err) {
+    return {}
+  }
+}]
 // 请求拦截器
 request.interceptors.request.use(
   function (config) {
@@ -21,10 +27,6 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
   function (response) {
-    return response
-  },
-  function (error) {
-    return Promise.reject(error)
   }
 )
 
