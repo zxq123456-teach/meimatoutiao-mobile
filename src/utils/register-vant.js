4deb7939b80334893c95dba5cfe542d9fbb6bcf2
zxq123456-teach <1060604915@qq.com>
2019-11-30 20:04:09 +0800

    登录-页面结构

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -3,10 +3,14 @@ import Vue from 'vue'
 import {
   Button,
   Cell,
-  CellGroup
+  CellGroup,
+  Field,
+  NavBar
 } from 'vant'
 
 Vue
   .use(Button)
   .use(Cell)
   .use(CellGroup)
+  .use(Field)
+  .use(NavBar)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class='login'>登陆组件</div>
+  <div class="login">
+    <!-- 导航栏 -->
+    <van-nav-bar title="登录" />
+    <!-- 导航栏 -->
+
+    <!-- 表单 -->
+    <van-cell-group>
+      <van-field
+        label="手机号"
+        placeholder="请输入手机号"
+      />
+
+      <van-field
+        label="验证码"
+        placeholder="请输入验证码"
+      />
+    </van-cell-group>
+    <!-- /表单 -->
+
+    <!-- 登录按钮 -->
+    <div class="login-btn-box">
+      <van-button type="info">登录</van-button>
+    </div>
+    <!-- /登录按钮 -->
+  </div>
 </template>
 
 <script>
