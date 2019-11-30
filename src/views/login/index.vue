423517b5e04eb7ff10e3c67bf16c8a62f3fdc212
zxq123456-teach <1060604915@qq.com>
2019-11-30 20:28:35 +0800

    登录-实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,35 +9,60 @@
       <van-field
         label="手机号"
         placeholder="请输入手机号"
+        v-model="user.mobile"
+        required
+        clearable
       />
 
       <van-field
         label="验证码"
         placeholder="请输入验证码"
+        v-model="user.code"
+        required
+        clearable
       />
     </van-cell-group>
     <!-- /表单 -->
 
     <!-- 登录按钮 -->
     <div class="login-btn-box">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click='onLogin'>登录</van-button>
     </div>
     <!-- /登录按钮 -->
   </div>
 </template>
 
 <script>
+import request from '@/utils/request'
 export default {
   name: 'LoginPage',
   components: {},
   prop: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      try {
+        const res = await request({
+          method: 'POST',
+          url: '/app/v1_0/authorizations',
+          data: this.user
+        })
+        console.log(res, '登陆成功')
+      } catch (err) {
+        console.log(err, '登陆失败')
+      }
+    }
+  }
 }
 </script>
 
