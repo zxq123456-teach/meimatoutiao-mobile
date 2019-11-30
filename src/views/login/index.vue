2422c4f6bea9aa42c21d8b456320062ff434d899
zxq123456-teach <1060604915@qq.com>
2019-11-30 19:41:57 +0800

    登录-创建登录组件并配置路由

---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='login'>登陆组件</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  components: {},
+  prop: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
