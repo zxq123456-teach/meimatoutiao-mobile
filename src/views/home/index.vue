aeaa8512c10b3c8028c6381a0c07d60a52055f4f
zxq123456-teach <1060604915@qq.com>
2019-12-03 17:10:34 +0800

    首页-展示文章列表-使用真实数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,48 +1,46 @@
 <template>
   <div>
-      <van-nav-bar title='首页' />
+    <van-nav-bar title='首页' />
 
-      <van-tabs v-model="active">
-        <van-tab
-            :title="channel.name"
-            v-for="channel in channels"
-            :key="channel.id">
-
-            <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-              <van-list
-                v-model="loading"
-                :finished="channel.finished"
-                finished-text="没有更多了"
-                @load="onLoad">
-
-                <p>{{ channel.name }}</p>
-                <van-cell
-               v-for="item in channel.articles"
-                :key="item"
-                :title="item"/>
-
-              </van-list>
-            </van-pull-refresh>
-            <!-- /文章列表 -->
-         </van-tab>
-      </van-tabs>
+    <van-tabs v-model="active">
+      <van-tab
+        :title="channel.name"
+        v-for="channel in channels"
+        :key="channel.id"
+      >
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <van-list
+            v-model="loading"
+            :finished="channel.finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell
+              v-for="article in channel.articles"
+              :key="article.art_id.toString()"
+              :title="article.title"
+            />
+          </van-list>
+        </van-pull-refresh>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/user'
+import { getArticles } from '@/api/article'
 export default {
   name: 'HomePage',
   components: {},
   props: {},
   data () {
     return {
-      active: 0,
-      list: [],
-      loading: false,
-      finished: false,
-      isLoading: false,
-      channels: []
+      active: 0, // 控制激活的标签页
+      loading: false, // 加载更多的转圈圈
+      isLoading: false, // 下拉刷新的转圈圈
+      channels: []// 频道列表
+
     }
   },
   computed: {},
@@ -52,32 +50,36 @@ export default {
     this.loadUserChannels()
   },
   methods: {
-    onLoad () {
-    // 获取当前频道
-    // this.active 双向绑定了标签页组件，该数据正好对应频道的索引
+    async onLoad () {
+      // 当前频道
       const activeChannel = this.channels[this.active]
 
-      // 获取当前频道的文章列表
+      // 当前频道的的文章列表
       const articles = activeChannel.articles
 
-      // 1. 请求加载数据
-      setTimeout(() => {
-        // 2. 将数据添加到当前频道的文章列表中
-        for (let i = 0; i < 10; i++) {
-          articles.push(articles.length + 1)
-        }
+      // 1. 请求获取数据
+      const res = await getArticles({
+        channel_id: activeChannel.id, // 频道 id
+        timestamp: activeChannel.timestamp || Date.now(), // 获取下一页数据的时间戳，Date.now() 表示获取当前最新数据
+        with_top: 1
+      })
 
-        // 3. 将 loading 设置为 false
-        // 加载状态结束
-        // 关闭本次的 loading 状态，它会判定当前数据是否够一屏，不够就继续自动 onLoad
-        this.loading = false
+      // 2. 将数据添加到当前频道的文章列表中
+      // ...数组，数组的展开操作符，它会把数组元素一个一个的拿出来，传递给使用的位置
+      articles.push(...res.data.data.results)
 
-        // 4. 判断如果没有数据了，将当前频道的结束状态 finished 设置为 true
-        if (articles.length >= 40) {
-        // 如果后面没有数据了，将 finished 设置为 true，之后不再触发 onLoad
-          activeChannel.finished = true
-        }
-      }, 1000)
+      // 3. 将 loading 设置为 false
+      this.loading = false
+
+      // 4. 判断还有下一页数据，则更新获取下一个数据的时间戳
+      //    如果没有了，则将 finished 设置为 true，不再加载更多了
+      const preTimestamp = res.data.data.pre_timestamp
+      if (preTimestamp) {
+        // 更新获取下一页数据的时间戳
+        activeChannel.timestamp = preTimestamp
+      } else {
+        activeChannel.finished = true
+      }
     },
 
     onRefresh () {
@@ -93,6 +95,7 @@ export default {
       channels.forEach(channel => {
         channel.articles = [] // 频道的文章列表
         channel.finished = false // 频道的加载结束状态
+        channel.timestamp = null // 用于获取频道下一页数据的时间戳
       })
       this.channels = channels
     }
